6a8613ff345eb155dde6fb6ae8e80889e8a3587b
Deniz Shabani <44246548+DenizShabani@users.noreply.github.com>
2022-02-12 00:52:54 +0100

    Update

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 from telethon.sync import TelegramClient
 from telethon.tl.types import UserStatusRecently
 import csv
+from time import sleep
+from telethon.tl.functions.channels import GetFullChannelRequest
 
-API_ID = 0000000 # API_ID as an integer
-API_HASH = "0000000000000000000" # API_HASH as a string
-PHONE = "000000000" # Phone number as a string without + or 00 /Ex: 12025550163
-GROUP_NAME = "group_username" # Group username /Ex: for t.me/random then write random
+API_ID = 00000000 # API_ID as an integer
+API_HASH = "00000000000000000000" # API_HASH as a string
+PHONE = "00000000000000000000" # Phone number as a string without + or 00 /Ex: 12025550163
+GROUP_NAME = "" # Group username /Ex: for t.me/random then write random
 
 c = TelegramClient("session", API_ID,API_HASH)
 c.start(PHONE)
 group = c.get_entity(GROUP_NAME)
-members = c.get_participants(group)
-print(f"\n{len(members)} users were scraped.\n")
 
 choice = input("Would you like to only obtain the users last seen recently? [y/n] ")
-if choice == "y" or "Y":
-    new_scraped = []
-    for user in members:
-        if user.deleted or user.bot or user.scam or not user.status == UserStatusRecently():
-            user = []
-        else:
-            new_scraped.append(user)
-    scraped = new_scraped
+members = []
+members = c.iter_participants(group)
 
+channel_full_info = c(GetFullChannelRequest(group))
+cont = channel_full_info.full_chat.participants_count
 
 with open("members\\members.csv", "w", encoding='UTF-8') as f:
     writer = csv.writer(f, delimiter=",", lineterminator="\n")
     writer.writerow(['username', 'user id', 'access hash', 'group', 'group id'])
-    if choice == 'y' or 'Y':
-        for member in members:
+    if choice == 'y' or choice =='Y':
+        for index,member in enumerate(members):
+            print(f"{index+1}/{cont}", end="\r")
+            if index%100 == 0:
+                sleep(1)
             if member.status == UserStatusRecently():
                 if member.username:
                     username = member.username
@@ -36,7 +35,10 @@ with open("members\\members.csv", "w", encoding='UTF-8') as f:
                     username = ''
                 writer.writerow([username, member.id, member.access_hash, group.title, group.id])
     else:
-        for member in members:
+        for index,member in enumerate(members):
+            print(f"{index+1}/{cont}", end="\r")
+            if index%100 == 0:
+                sleep(1)
             if member.username:
                 username = member.username
             else:
@@ -44,4 +46,4 @@ with open("members\\members.csv", "w", encoding='UTF-8') as f:
             writer.writerow([username, member.id, member.access_hash, group.title, group.id])
 f.close()
 
-print(f"\n{len(new_scraped)} users are left and saved in the csv file.\n")
+print(f"\nUsers saved in the csv file.\n")
